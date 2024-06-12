1b4f4c5d59d417acca185d2dff736e8f8705f2c6
ekd594ff <ekd594ff@gmail.com>
2024-06-12 16:01:47 +0900

    remove annotation

--- src/main/java/com/desk8432/project/controller/InsertMember.java
+++ src/main/java/com/desk8432/project/controller/InsertMember.java
@@ -14,7 +14,6 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @WebServlet("/insert")
-@MultipartConfig
 public class InsertMember extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
