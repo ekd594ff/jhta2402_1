61597167e10e6797bd99ec20fedd0e9d89d21831
jin0815b <wlsgus25843@gmail.com>
2024-06-18 13:45:03 +0900

    scheduleList

--- src/main/java/com/desk8432/project/dao/schedule/UpdateScheduleDAO.java
+++ src/main/java/com/desk8432/project/dao/schedule/UpdateScheduleDAO.java
@@ -1,22 +1,22 @@
 package com.desk8432.project.dao.schedule;
 
-import com.desk8432.project.dto.schedule.UpdateScheduleDTO;
+import com.desk8432.project.dto.schedule.ScheduleDTO;
 import com.desk8432.project.mybatis.MybatisConnectionFactory;
 import org.apache.ibatis.session.SqlSession;
 
 public class UpdateScheduleDAO {
-    public String getGroupNameDTO(UpdateScheduleDTO updateScheduleDTO) {
+    public String getGroupNameDTO(ScheduleDTO scheduleDTO) {
         String groupname = null;
         SqlSession sqlSession = MybatisConnectionFactory.getSqlSession();
-        groupname = sqlSession.selectOne("getGroupName", updateScheduleDTO);
+        groupname = sqlSession.selectOne("getGroupName", scheduleDTO);
         sqlSession.close();
         return groupname;
     }
 
-    public boolean updateSchedule(UpdateScheduleDTO updateScheduleDTO) {
+    public boolean updateSchedule(ScheduleDTO scheduleDTO) {
         int result = 0;
         SqlSession sqlSession = MybatisConnectionFactory.getSqlSession();
-        result = sqlSession.update("updateSchedule", updateScheduleDTO);
+        result = sqlSession.update("updateSchedule", scheduleDTO);
         sqlSession.close();
         return result > 0;
     }
--- src/main/java/com/desk8432/project/dto/schedule/ScheduleDTO.java
+++ src/main/java/com/desk8432/project/dto/schedule/ScheduleDTO.java
@@ -3,7 +3,6 @@ package com.desk8432.project.dto.schedule;
 
 import lombok.*;
 
-import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
 @Getter
@@ -12,13 +11,13 @@ import java.util.Date;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class UpdateScheduleDTO {
+public class ScheduleDTO {
     private int id;
     private int groupID;
+    private String groupname;
     private String editor;
     private String title;
     private String content;
     private Date startDate;
     private Date endDate;
-    private String groupname;
 }
