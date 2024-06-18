8c55f2c658266673c0f73dda11194fe8afd1aa33
typhoon0678 <typhoon0678@gmail.com>
2024-06-18 16:09:58 +0900

    check login (session, cookie)

--- src/main/java/com/desk8432/project/filter/LoginCheckFilter.java
+++ src/main/java/com/desk8432/project/filter/LoginCheckFilter.java
@@ -1,22 +1,30 @@
 package com.desk8432.project.filter;
 
+import com.desk8432.project.dto.member.LoginMemberDTO;
 import jakarta.servlet.*;
 import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
 
-@WebFilter({"/mypage/*"})
-public class MyPageFilter implements Filter {
+@WebFilter({"/mypage/*", "/schedule/*"})
+public class LoginCheckFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
             throws IOException, ServletException {
         HttpServletRequest httpRequest = (HttpServletRequest) request;
         HttpServletResponse httpResponse = (HttpServletResponse) response;
-        if (readCookie(httpRequest, "username") != null) {
+
+        Cookie usernameCookie = readCookie(httpRequest, "username");
+        HttpSession session = httpRequest.getSession();
+        LoginMemberDTO loginMemberDTO = (LoginMemberDTO) session.getAttribute("member");
+
+        if (usernameCookie != null && loginMemberDTO != null
+                && loginMemberDTO.getUsername().equals(usernameCookie.getValue())) {
             chain.doFilter(request, response);
         } else {
             httpResponse.sendRedirect(httpRequest.getContextPath() + "/signin");
