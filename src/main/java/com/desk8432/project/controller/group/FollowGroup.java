d5bf21633959c745c203239e917df883486b2943
typhoon0678 <typhoon0678@gmail.com>
2024-06-20 15:03:02 +0900

    add group/follow getlist

--- src/main/java/com/desk8432/project/controller/group/FollowGroup.java
+++ src/main/java/com/desk8432/project/controller/group/FollowGroup.java
@@ -2,6 +2,8 @@ package com.desk8432.project.controller.group;
 
 import com.desk8432.project.dao.group.FollowGroupDAO;
 import com.desk8432.project.dto.group.FollowRequestDTO;
+import com.desk8432.project.dto.group.GroupViewResponseDTO;
+import com.desk8432.project.dto.group.SearchGroupDTO;
 import com.desk8432.project.util.CookieManager;
 import com.google.gson.Gson;
 import jakarta.servlet.ServletException;
@@ -11,11 +13,32 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @WebServlet("/group/follow")
 public class FollowGroup extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("application/json; charset=utf-8");
+        Gson gson = new Gson();
+
+        String username = CookieManager.readCookie(req, "username");
+
+        FollowGroupDAO followGroupDAO = new FollowGroupDAO();
+        List<SearchGroupDTO> searchGroupDTOList = followGroupDAO.getFollowGroup(username);
+
+        if (searchGroupDTOList != null) {
+            GroupViewResponseDTO groupViewResponseDTO = getGroupViewResponseDTO(searchGroupDTOList, username);
+            resp.setStatus(HttpServletResponse.SC_OK);
+            resp.getWriter().write(gson.toJson(groupViewResponseDTO));
+        } else {
+            resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        }
+    }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -43,7 +66,6 @@ public class FollowGroup extends HttpServlet {
     @Override
     protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("application/json; charset=utf-8");
-        Gson gson = new Gson();
 
         String username = CookieManager.readCookie(req, "username");
         Long groupID = Long.parseLong(req.getParameter("groupID"));
@@ -60,5 +82,23 @@ public class FollowGroup extends HttpServlet {
         } else {
             resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
         }
+    }
+
+    private GroupViewResponseDTO getGroupViewResponseDTO(List<SearchGroupDTO> searchGroupDTOList, String username) {
+        List<SearchGroupDTO> myGroupList = new ArrayList<>();
+        List<SearchGroupDTO> followList = new ArrayList<>();
+
+        for (SearchGroupDTO searchGroupDTO : searchGroupDTOList) {
+            if (searchGroupDTO.getCreator().equals(username)) {
+                myGroupList.add(searchGroupDTO);
+            } else {
+                followList.add(searchGroupDTO);
+            }
+        }
+
+        return GroupViewResponseDTO.builder()
+                .myGroups(myGroupList)
+                .followGroups(followList)
+                .build();
     }
 }
--- src/main/java/com/desk8432/project/dao/group/FollowGroupDAO.java
+++ src/main/java/com/desk8432/project/dao/group/FollowGroupDAO.java
@@ -1,10 +1,23 @@
 package com.desk8432.project.dao.group;
 
 import com.desk8432.project.dto.group.FollowRequestDTO;
+import com.desk8432.project.dto.group.SearchGroupDTO;
 import com.desk8432.project.mybatis.MybatisConnectionFactory;
 import org.apache.ibatis.session.SqlSession;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FollowGroupDAO {
+
+    public List<SearchGroupDTO> getFollowGroup(String username) {
+        SqlSession sqlSession = MybatisConnectionFactory.getSqlSession();
+        List<SearchGroupDTO> groupDTOList = sqlSession.selectList("getFollowGroup", username);
+        sqlSession.close();
+        return groupDTOList;
+    }
+
+
     public boolean addFollowGroup(FollowRequestDTO followRequestDTO) {
         SqlSession sqlSession = MybatisConnectionFactory.getSqlSession();
         int result = sqlSession.insert("insertFollowGroup", followRequestDTO);
