2479a4620b5523c1b94032e34a81daba6672ebda
typhoon0678 <typhoon0678@gmail.com>
2024-06-21 13:46:12 +0900

    fix: match groupform CreateGroup groupImageUpload refactor imageUploadMethod

--- src/main/java/com/desk8432/project/controller/group/CrudGroup.java
+++ src/main/java/com/desk8432/project/controller/group/CrudGroup.java
@@ -1,58 +1,73 @@
-package com.desk8432.project.controller.member;
+package com.desk8432.project.controller.group;
 
+import com.desk8432.project.dao.group.InsertGroupDAO;
 import com.desk8432.project.dao.member.UpdateDAO;
+import com.desk8432.project.dto.group.InsertGroupDTO;
 import com.desk8432.project.dto.member.UpdateImageUrlDTO;
 import com.desk8432.project.util.CookieManager;
 import com.google.gson.Gson;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.MultipartConfig;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.Part;
 
-
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
-
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 import static com.desk8432.project.util.UploadImage.getImageDTO;
 import static com.desk8432.project.util.UploadImage.uploadImage;
 
-@WebServlet("/update/imageUrl")
-@MultipartConfig
-public class UpdateImageUrl03 extends HttpServlet {
+@WebServlet("/group/crud")
+public class CrudGroup extends HttpServlet {
+
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
         String username = CookieManager.readCookie(req, "username");
+        String groupName = req.getParameter("name");
+        String content = req.getParameter("content");
         Part image = req.getPart("image");
 
-//        String username = req.getParameter("username");
+        Map<String, String> resultMap = new HashMap<>();
+        Gson gson = new Gson();
+
+        UpdateImageUrlDTO updateImageUrlDTO = getImageDTO(
+                image, username + "_" + groupName, getServletConfig());
+
+        InsertGroupDTO insertGroupDTO = InsertGroupDTO.builder()
+                .image_url(updateImageUrlDTO.getImageUrl())
+                .name(groupName)
+                .content(content)
+                .creator(username)
+                .build();
+
         UpdateDAO updateDAO = new UpdateDAO();
+        InsertGroupDAO insertGroupDAO = new InsertGroupDAO();
 
-        Gson outGson = new Gson();
-        Map<String,String> resultMap = new HashMap<>(); //결과값 반환
-
-        UpdateImageUrlDTO updateImageUrlDTO = getImageDTO(image,username, getServletConfig());
-
-        CompletableFuture<Void> memberImageUrlUpdateFuture = CompletableFuture.runAsync(() ->{
-            if (updateDAO.updateImageUrl(updateImageUrlDTO)) {
-                //member db에 이미지 경로 수정
-                resultMap.put("url","ok");
+        // group DB에 저장
+        CompletableFuture<Void> memberImageUrlUpdateFuture = CompletableFuture.runAsync(() -> {
+            if (insertGroupDAO.insertGroup(insertGroupDTO)) {
+                System.out.println("Insert group successful");
+                resultMap.put("url", "ok");
             }
         });
-        CompletableFuture<Void> uploadImageFuture = CompletableFuture.runAsync(() ->{
-            uploadImage(image,updateImageUrlDTO.getLocation(), updateImageUrlDTO.getFileName()); //이미지 메인서버에 저장
+
+        // 서버에 이미지 저장
+        CompletableFuture<Void> uploadImageFuture = CompletableFuture.runAsync(() -> {
+            uploadImage(image, updateImageUrlDTO.getLocation(), updateImageUrlDTO.getFileName()); //이미지 메인서버에 저장
         });
+
+        // File 테이블에 있는지 확인
         CompletableFuture<Boolean> isFileImageFuture = CompletableFuture.supplyAsync(() -> {
-            // 비동기 작업
-            return updateDAO.isFile(updateImageUrlDTO); // Boolean 값 반환
+            return updateDAO.isFile(updateImageUrlDTO);
         });
+        // 있는지 확인 후에 저장 or 수정
         isFileImageFuture.thenApply(result -> {
             if (result) {
                 boolean isUpdateFile = updateDAO.updateFile(updateImageUrlDTO);
@@ -73,20 +88,18 @@ public class UpdateImageUrl03 extends HttpServlet {
                 }
                 return isInsertFile;
             }
-            // thenApply()는 새로운 값을 반환해야 합니다.
         });
 
         try {
             memberImageUrlUpdateFuture.get();
             uploadImageFuture.get();
             isFileImageFuture.get();
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        } catch (ExecutionException e) {
+        } catch (InterruptedException | ExecutionException e) {
             throw new RuntimeException(e);
         }
 
-        String resultJson = outGson.toJson(resultMap);
+
+        String resultJson = gson.toJson(resultMap);
         resp.setContentType("application/json; charset=utf-8");
         PrintWriter out = resp.getWriter();
         out.println(resultJson);
