2854b48d0e6679ce8fe4771d379bf942c284b9c1
jin0815b <wlsgus25843@gmail.com>
2024-06-26 15:36:01 +0900

    fix/login

--- src/main/java/com/desk8432/project/controller/member/LoginController.java
+++ src/main/java/com/desk8432/project/controller/member/LoginController.java
@@ -45,15 +45,15 @@ public class LoginController extends HttpServlet {
         Map<String,String> resultMap = new HashMap<>();
 
         if (hashPW != null) {
-            boolean checkLogin = BCrypt.checkpw(password, hashPW);
+            LoginMemberDTO loginMemberDTO = loginDAO.loginMember(
+                    LoginDTO.builder()
+                            .username(username)
+                            .password(hashPW)
+                            .build());
+
+            boolean checkLogin = ((loginMemberDTO != null) && BCrypt.checkpw(password, hashPW));
 
             if (checkLogin) {
-                LoginMemberDTO loginMemberDTO = loginDAO.loginMember(
-                        LoginDTO.builder()
-                        .username(username)
-                        .password(hashPW)
-                        .build());
-
                 // myPageDTO를 쿠키 저장 / 세션에 저장
                 HttpSession session = req.getSession();
                 session.setAttribute("member", loginMemberDTO);
